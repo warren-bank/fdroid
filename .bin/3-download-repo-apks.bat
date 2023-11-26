6b43339b02d66c59930a90fe3ad3968fe4fe22d1
Warren R Bank <warren.r.bank@gmail.com>
2023-11-26 15:35:47 -0800

    add: F-Droid v1.18.0-1.0.0 and v1.12.1-1.0.0

--- .bin/3-download-repo-apks.bat
+++ .bin/3-download-repo-apks.bat
@@ -108,6 +108,10 @@ call :do_download "https://github.com/warren-bank/fork-Android-Httper/releases/d
 rem :: https://github.com/warren-bank/Android-RTSP-IPCam-Viewer/releases
 call :do_download "https://github.com/warren-bank/Android-RTSP-IPCam-Viewer/releases/download/v02.04.01/RTSP-IPCam-Viewer-002.04.01-16API-withAllLanguageTranslations-release.apk"
 
+rem :: https://github.com/warren-bank/fork-Android-fdroidclient/releases
+call :do_download "https://github.com/warren-bank/fork-Android-fdroidclient/releases/download/fork/1.18.0/v1.0.0/fdroidclient-1.18.0-fork-v1.0.0-full-release.apk"
+call :do_download "https://github.com/warren-bank/fork-Android-fdroidclient/releases/download/fork/1.12.1/v1.0.0/fdroidclient-1.12.1-fork-v1.0.0-full-release.apk"
+
 goto :done
 
 :do_download
