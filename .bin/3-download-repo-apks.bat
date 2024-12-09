f5e7acec08deeb60ccea8f14fe753b4b505fed31
Warren R. Bank <warren.r.bank@gmail.com>
2024-12-09 02:51:58 -0800

    update: Bookmarks v2.0.3

--- .bin/3-download-repo-apks.bat
+++ .bin/3-download-repo-apks.bat
@@ -58,8 +58,8 @@ call :do_download "https://github.com/warren-bank/Android-Tiny-Television-Time-T
 call :do_download "https://github.com/warren-bank/Android-Tiny-Television-Time-Tracker/releases/download/tmdb/008.00.26-09API/TV-Tracker-008.00.26-09API-withAllLanguageTranslations-release.apk"
 
 rem :: https://github.com/warren-bank/Android-Bookmarks/releases
-call :do_download "https://github.com/warren-bank/Android-Bookmarks/releases/download/002.00.01-09API/Bookmarks-002.00.01-09API-withUserRuntimePermissions-english-release.apk"
-call :do_download "https://github.com/warren-bank/Android-Bookmarks/releases/download/002.00.01-09API/Bookmarks-002.00.01-09API-withUserRuntimePermissions-withAllLanguageTranslations-release.apk"
+call :do_download "https://github.com/warren-bank/Android-Bookmarks/releases/download/002.00.03-09API/Bookmarks-002.00.03-09API-withUserRuntimePermissions-english-release.apk"
+call :do_download "https://github.com/warren-bank/Android-Bookmarks/releases/download/002.00.03-09API/Bookmarks-002.00.03-09API-withUserRuntimePermissions-withAllLanguageTranslations-release.apk"
 
 rem :: https://github.com/warren-bank/Android-MyPlaces/releases
 call :do_download "https://github.com/warren-bank/Android-MyPlaces/releases/download/v05.00.05/MyPlaces-release.apk"
