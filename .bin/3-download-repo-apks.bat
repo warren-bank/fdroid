c2f66f011857e60c54a57257d2605809195a8de4
Warren R. Bank <warren.r.bank@gmail.com>
2024-12-09 01:37:59 -0800

    update: Tiny Television Time Tracker (TV Tracker) v8.0.26

--- .bin/3-download-repo-apks.bat
+++ .bin/3-download-repo-apks.bat
@@ -53,8 +53,8 @@ call :do_download "https://github.com/warren-bank/Android-NodeJS-Frontend/releas
 call :do_download "https://github.com/warren-bank/Android-NodeJS-Frontend/releases/download/v1.0.2/NodeJS-Frontend-001.00.02-19API-withAllLanguageTranslations-x86_64-release.apk"
 
 rem :: https://github.com/warren-bank/Android-Tiny-Television-Time-Tracker/releases
-call :do_download "https://github.com/warren-bank/Android-Tiny-Television-Time-Tracker/releases/download/tmdb/008.00.24-09API/TV-Tracker-008.00.24-09API-english-release.apk"
-call :do_download "https://github.com/warren-bank/Android-Tiny-Television-Time-Tracker/releases/download/tmdb/008.00.24-09API/TV-Tracker-008.00.24-09API-withAllLanguageTranslations-release.apk"
+call :do_download "https://github.com/warren-bank/Android-Tiny-Television-Time-Tracker/releases/download/tmdb/008.00.26-09API/TV-Tracker-008.00.26-09API-english-release.apk"
+call :do_download "https://github.com/warren-bank/Android-Tiny-Television-Time-Tracker/releases/download/tmdb/008.00.26-09API/TV-Tracker-008.00.26-09API-withAllLanguageTranslations-release.apk"
 
 rem :: https://github.com/warren-bank/Android-Bookmarks/releases
 call :do_download "https://github.com/warren-bank/Android-Bookmarks/releases/download/002.00.01-09API/Bookmarks-002.00.01-09API-withUserRuntimePermissions-english-release.apk"
